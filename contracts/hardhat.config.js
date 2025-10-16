cc5abe58b45aee0784278847b2e9db98206db497
Joseph Emmanuel <117820245+Fmsticks2@users.noreply.github.com>
2025-10-16 18:32:27 +0100

    create rampvault.sol

---
+++ contracts/hardhat.config.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+require('@nomicfoundation/hardhat-toolbox')
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: '0.8.23',
+    settings: { optimizer: { enabled: true, runs: 200 } },
+  },
+  networks: {
+    hardhat: {},
+  },
+}
